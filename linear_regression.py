b4643ae7dde5fe9e167f9227e89b5b4cdcbf0211
Ian Gagliardi <iangagliardi@gmail.com>
2026-02-11 16:40:54 +0100

    Update

--- linear_regression.py
+++ linear_regression.py
@@ -1,20 +1,54 @@
 import numpy as np
-import scipy.stats as stats
 
 class LinearRegression:
 
-    def __init__(self, X, y):
-        self.X = np.array(X) # Features
-        self.y = np.array(y) # Target
-        self.d = self.X.shape[1]# Number of features
-        self.n = self.X.shape[0] # Number of samples
+    def __init__(self):
+        self.b = None   # coefficients
+        self.d = None   # number of features
+        self.n = None   # sample size
 
+    def fit(self, X, Y):
 
-    def least_squares_mean(self):
-        return np.mean(self.y)
-    
-    def standard_deviation(self):
-        return np.std(self.y, ddof=1)
-    
-if __name__ == "__main__":
-    pass
+        # Add column of 1s for intercept (beta0)
+        ones = np.ones((X.shape[0], 1))
+        X_design = np.hstack((ones, X))
+
+        # Store n and d
+        self.n = X_design.shape[0]
+        self.d = X_design.shape[1] - 1
+
+        # Least squares solution
+        XtX = X_design.T @ X_design
+        XtX_inv = np.linalg.inv(XtX)
+        XtY = X_design.T @ Y
+        self.b = XtX_inv @ XtY
+
+        return self.b
+
+    def predict(self, X):
+  
+        # Predict using learned coefficients
+        ones = np.ones((X.shape[0], 1))
+        X_design = np.hstack((ones, X))
+        return X_design @ self.b
+
+    def sample_variance(self, X, Y):
+
+        # Unbiased estimator of variance
+        Y_pred = self.predict(X)
+        residuals = Y - Y_pred
+        SSE = np.sum(residuals**2)
+
+        variance = SSE / (self.n - self.d - 1)
+        return variance
+
+    def standard_deviation(self, X, Y):
+
+        variance = self.sample_variance(X, Y)
+        return np.sqrt(variance)
+
+    def rmse(self, X, Y):
+
+        Y_pred = self.predict(X)
+        mse = np.mean((Y - Y_pred)**2)
+        return np.sqrt(mse)
